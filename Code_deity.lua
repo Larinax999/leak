e05fe36e69e217459cabc1547ed66f5f73b3f7c5
Larina <49904492+Larinax999@users.noreply.github.com>
2023-01-04 17:47:34 +0700

    shit code

---
+++ Code_deity.lua
@@ -0,0 +1,18 @@
+if TypeGame == "BloxFruit" then
+return {
+"Sub2Fer999",
+"Enyu_is_Pro",
+'Magicbus',
+'JCWK',
+'Starcodeheo',
+'Bluxxy',
+'THEGREATACE',
+'SUB2GAMERROBOT_EXP1' ,
+'Sub2OfficialNoobie' ,
+'StrawHatMaine' ,
+'SUB2NOOBMASTER123' ,
+'Sub2Daigrock' ,
+"Axiore" ,
+'TantaiGaming' 
+}
+end
